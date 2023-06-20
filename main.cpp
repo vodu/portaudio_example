42132732834595edee8b85391d96905f640357ae
Volker Dümke <volker@mimi.io>
2023-06-20 15:26:34 +0200

    adds sine tone, adds sndfile dep, trying to modify vcpkg triplet

--- main.cpp
+++ main.cpp
@@ -1,48 +1,89 @@
 #include <iostream>
+//#include <sndfile.h>
 #include <portaudio.h>
 
 #define SAMPLE_RATE (44100)
+#define FRAMES_PER_BUFFER (256)
+#define NUM_OUTPUT_CHANNEL (1)
 
-/* This routine will be called by the PortAudio engine when audio is needed.
-** It may called at interrupt level on some machines so don't do anything
-** that could mess up the system like calling malloc() or free().
-*/
-static int patestCallback( const void *inputBuffer, void *outputBuffer,
-                            unsigned long framesPerBuffer,
-                            const PaStreamCallbackTimeInfo* timeInfo,
-                            PaStreamCallbackFlags statusFlags,
-                            void *userData )
+#ifndef M_PI
+#define M_PI (3.14159265)
+#endif
+
+
+
+
+typedef struct {
+    float phase;
+} paTestData;
+
+
+static int patestCallback(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo *timeInfo, PaStreamCallbackFlags statusFlags, void *userData)
 {
+    paTestData *data = (paTestData *)userData;
+    
+    float *out = (float*) outputBuffer;
+
+    for (unsigned long i = 0; i < framesPerBuffer; ++i)
+    {
+        out[i] = sin(data->phase * 2 * M_PI) * 0.1;
+        if (data->phase >= 1.0) data->phase -= 1.0;
+        data->phase += (1. / ((double) SAMPLE_RATE / 1000));
+    }
+
     return paContinue;
 }
 
 int main()
 {
-    std::cout << "--- portaudio test ---" << std::endl;
-    
-    auto err = Pa_Initialize();
+    PaError err;
+
+    /* Initialize PortAudio */   
+    err = Pa_Initialize();
     if (err != paNoError) goto error;
 
+    /* Get default output device */
+    PaStreamParameters outputParameters; 
+    outputParameters.device = Pa_GetDefaultOutputDevice();
+    if (outputParameters.device == paNoDevice) {
+        std::cout << "Error: No default output device.\n" << std::endl;
+        goto error;
+    }
+    outputParameters.channelCount = NUM_OUTPUT_CHANNEL;
+    outputParameters.sampleFormat = paFloat32;
+    outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
+    outputParameters.hostApiSpecificStreamInfo = NULL;
+
+    paTestData data = { 0 };
     PaStream *stream;
-    uint16_t frames_per_buffer = 256;
-    uint16_t num_output_chan = 2;
     /* Open an audio I/O stream. */
-    err = Pa_OpenDefaultStream( &stream,
-                                0, /* no input channels */
-                                num_output_chan,
-                                paFloat32,  
-                                SAMPLE_RATE,
-                                frames_per_buffer,  
-                                patestCallback, 
-                                NULL ); /* custom data*/
+    err = Pa_OpenStream(&stream,
+                        NULL, /* no input */
+                        &outputParameters,
+                        SAMPLE_RATE,
+                        FRAMES_PER_BUFFER,
+                        paClipOff, /* we won't output out of range samples so don't bother clipping them */
+                        patestCallback,
+                        &data); /* custom data*/
     if (err != paNoError) goto error;
 
+    err = Pa_StartStream(stream);
+    if (err != paNoError) goto error;
+
+    Pa_Sleep(1000);
+
+    err = Pa_StopStream(stream);
+    if (err != paNoError) goto error;
+
+    err = Pa_CloseStream(stream);
+    if (err != paNoError) goto error;
 
     err = Pa_Terminate();
     if (err != paNoError) goto error;
     
+    return 0;
+
 error:
     std::cout << "PortAudio error: " << Pa_GetErrorText(err) << std::endl;
-
-    return 0;
+    return 1;
 }
